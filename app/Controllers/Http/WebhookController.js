4b50f23f05d7b7b8015467961c5813f314441aec
livissnack <brucesnack@outlook.com>
2019-03-14 14:47:31 +0800

    submit code

--- app/Controllers/Http/WebhookController.js
+++ app/Controllers/Http/WebhookController.js
@@ -1,16 +1,8 @@
 'use strict'
 const Logger = use('Logger')
-const shell = use('shelljs')
-const Helpers = use('Helpers')
 
 class WebhookController {
   async index({request}) {
-    const shellUrl = `${Helpers.appRoot()}/deploy.sh`
-    shell.exec(shellUrl, (code, stdout, stderr) => {
-      Logger.info(`${new Date()} Exit code:`, code)
-      Logger.info(`${new Date()} Program output:`, stdout)
-      Logger.info(`${new Date()} 执行完毕！错误信息：？`, stderr)
-    })
     Logger.info(`${new Date()} 执行完毕！错误信息：？`, request.headers())
   }
 }
