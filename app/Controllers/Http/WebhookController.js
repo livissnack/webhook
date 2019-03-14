1247e7a22ee80184be075670714cfe7715cdbe85
livissnack <brucesnack@outlook.com>
2019-03-14 15:59:42 +0800

    submit code

--- app/Controllers/Http/WebhookController.js
+++ app/Controllers/Http/WebhookController.js
@@ -8,6 +8,14 @@ class WebhookController {
       data: []
     })
   }
+
+  async test({request, response }) {
+    response.json({
+      status: 'ok',
+      msg: '请求成功',
+      data: request.headers()
+    })
+  }
 }
 
 module.exports = WebhookController
